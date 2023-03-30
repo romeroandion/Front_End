d29cac0c1b3774b430dc5467263807a8384dfc31
Romero Andion <romeroandion2@gmail.com>
2023-03-30 08:31:55 -0300

    Add files via upload

---
+++ Gaming Zone/jquery-script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#botao-cadastrar').click(function(){
+
+        $('#form-cadastrar').slideToggle('slow');
+        $('#section-login').slideToggle('slow');
+        $('#botao-cadastrar').hide();
+    });
+});
